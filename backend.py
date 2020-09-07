e0e4d2e6117728070f70114c0829882d9d35e282
FayazDheen <fayazdheen786@gmail.com>
2020-09-08 00:05:55 +0530

    first commit

--- backend.py
+++ backend.py
@@ -14,38 +14,38 @@ data = result.json()
 
 # merged = data['paid'] + data['free']
 # print (merged)
-for pad in data['free']:
-    for each in pad:
-        print(each)
-    print()
-a = 0
-# for jobjectArr in merged:
-#     a+=1
-#     if jobjectArr['confName'] != "":
-#         print ("Conference Name : ",jobjectArr['confName'])
-#     if jobjectArr['confStartDate'] != "":
-#         print ("Conference Start Date : ",jobjectArr['confStartDate'])
-#     if jobjectArr['confEndDate'] != "":
-#         print ("Conference End Date : ",jobjectArr['confEndDate'])
-#     if jobjectArr['venue'] != "":
-#         print ("Venue : ",jobjectArr['venue'])
-#     if jobjectArr['city'] != "":
-#         print ("City : ",jobjectArr['city'])
-#     if jobjectArr['state'] != "":
-#         print ("State : ",jobjectArr['state'])
-#     if jobjectArr['country'] != "":
-#         print ("Country : ",jobjectArr['country'])
-#     if jobjectArr['entryType'] != "":
-#         print ("Entry Type : ",jobjectArr['entryType'])
-#     if jobjectArr['emailId'] != "":
-#         print ("Email ID : ",jobjectArr['emailId'])
-#     if jobjectArr['twitter_handle'] != "":
-#         print ("Twitter : ",jobjectArr['twitter_handle'])
-#     if jobjectArr['confUrl'] != "":
-#         print ("Conference URL : ",jobjectArr['confUrl'])
-#     if jobjectArr['confRegUrl'] != "":
-#         print ("Conference Registration URL : ",jobjectArr['confRegUrl'])
-#     print ()
+# for pad in data['free']:
+#     for each in pad:
+#         print(each)
+#     print()
+# a = 0
+for jobjectArr in merged:
+    a+=1
+    if jobjectArr['confName'] != "":
+        print ("Conference Name : ",jobjectArr['confName'])
+    if jobjectArr['confStartDate'] != "":
+        print ("Conference Start Date : ",jobjectArr['confStartDate'])
+    if jobjectArr['confEndDate'] != "":
+        print ("Conference End Date : ",jobjectArr['confEndDate'])
+    if jobjectArr['venue'] != "":
+        print ("Venue : ",jobjectArr['venue'])
+    if jobjectArr['city'] != "":
+        print ("City : ",jobjectArr['city'])
+    if jobjectArr['state'] != "":
+        print ("State : ",jobjectArr['state'])
+    if jobjectArr['country'] != "":
+        print ("Country : ",jobjectArr['country'])
+    if jobjectArr['entryType'] != "":
+        print ("Entry Type : ",jobjectArr['entryType'])
+    if jobjectArr['emailId'] != "":
+        print ("Email ID : ",jobjectArr['emailId'])
+    if jobjectArr['twitter_handle'] != "":
+        print ("Twitter : ",jobjectArr['twitter_handle'])
+    if jobjectArr['confUrl'] != "":
+        print ("Conference URL : ",jobjectArr['confUrl'])
+    if jobjectArr['confRegUrl'] != "":
+        print ("Conference Registration URL : ",jobjectArr['confRegUrl'])
+    print ()
 
 # print(type(merged))
 # print(a)
